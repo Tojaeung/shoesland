132dd94de3ebb8416933f95707b5116ef9e287e7
Tojaeung <kimwoon11@naver.com>
2022-07-03 20:02:34 +0900

    first commit

---
+++ backend/src/main/java/me/tojaeung/shoesland/ShoeslandApplication.java
@@ -0,0 +1,13 @@
+package me.tojaeung.shoesland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeslandApplication.class, args);
+	}
+
+}
